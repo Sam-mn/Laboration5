a2ad6dc51638d6724314279dab59ecf86c828a65
Karsten Tvarnö <vvarrior@gmail.com>
2024-11-12 13:42:47 +0100

    tests for 301 and 302

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -63,4 +63,76 @@ public class PaymentTest
         assertEquals(4960, test.getMonthlyAmount("20040323-1221", 0, 50, 90));
         assertEquals(9904, test.getMonthlyAmount("20040323-1221", 0, 100, 90));
     }
+
+    @Test
+    public void testFullTimeMaxIncome85812Sek301()
+    {
+        assertEquals(7088+2816, test.getMonthlyAmount("20040323-1221", 85812, 100, 90));
+    }
+
+    @Test
+    public void testFullTimeMaxIncome85813Sek301()
+    {
+        assertEquals(7088+2816, test.getMonthlyAmount("20040323-1221", 85813, 100, 90));
+    }
+
+    @Test
+    public void testFullTimeMaxIncome85814Sek301()
+    {
+        assertEquals(0, test.getMonthlyAmount("20040323-1221", 85814, 100, 90));
+    }
+
+    @Test
+    public void testFullTimeMaxIncome85000Sek301()
+    {
+        assertEquals(7088+2816, test.getMonthlyAmount("20040323-1221", 85000, 100, 90));
+    }
+
+    @Test
+    public void testFullTimeMaxIncome86000Sek301()
+    {
+        assertEquals(0, test.getMonthlyAmount("20040323-1221", 86000, 100, 90));
+    }
+
+    @Test
+    public void testMoreThanFullTimeMaxIncome85812Sek301()
+    {
+        assertEquals(7088+2816, test.getMonthlyAmount("20040323-1221", 85812, 101, 90));
+    }
+
+    @Test
+    public void testMoreThanFullTimeMaxIncome85814Sek301()
+    {
+        assertEquals(0, test.getMonthlyAmount("20040323-1221", 85813, 101, 90));
+    }
+
+    @Test
+    public void testHalfTimeMaxIncome128721Sek302()
+    {
+        assertEquals(3564+1396, test.getMonthlyAmount("20040323-1221", 128721, 50, 90));
+    }
+
+    @Test
+    public void testHalfTimeMaxIncome128722Sek302()
+    {
+        assertEquals(3564+1396, test.getMonthlyAmount("20040323-1221", 128722, 50, 90));
+    }
+
+    @Test
+    public void testHalfTimeMaxIncome128723Sek302()
+    {
+        assertEquals(0, test.getMonthlyAmount("20040323-1221", 128723, 50, 90));
+    }
+
+    @Test
+    public void testHalfTimeMaxIncome128000Sek302()
+    {
+        assertEquals(3564+1396, test.getMonthlyAmount("20040323-1221", 128000, 50, 90));
+    }
+
+    @Test
+    public void testHalfTimeMaxIncome129000Sek302()
+    {
+        assertEquals(0, test.getMonthlyAmount("20040323-1221", 129000, 50, 90));
+    }
 }
